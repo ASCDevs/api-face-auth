11cd3f84f6799473f9a4aaa8eff71bbb9e36ce29
Alexandre Santos <ale.santos49@gmail.com>
2021-10-07 23:50:21 -0300

    Arquivos iniciais da API

---
+++ api-face-auth/Controllers/FaceController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace api_face_auth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FaceController : Controller
+    {
+        [HttpGet]
+        public JsonResult statusFaces()
+        {
+            var dados = new { 
+                imgsCadastradas = 0,
+                dataStatus = DateTime.Now,
+                quatidadeUsers = 0
+            };
+
+            return Json(dados);
+        }
+
+        [HttpPost("register")]
+        public JsonResult registerFace([FromForm] IFormFile imagem)
+        {
+            if(imagem.Length > 0)
+            {
+
+            }
+            return Json(new { message = "Cadastro do Rosto do usuário em construção"});
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetFace(int id)
+        {
+            return Json(new { idRecebido = id, message="Api em construção"});
+        }
+    }
+}
---
+++ api-face-auth/Models/FaceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_face_auth.Models
+{
+    public class FaceModel
+    {
+        public int id { get; set; }
+        public DateTime dtCreated { get; set; }
+        public Byte[] imagem { get; set; }
+    }
+}
