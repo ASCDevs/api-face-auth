5d8b04f1e21c4f396612c6c0a06e6495ae71eab3
Alexandre Santos <ale.santos49@gmail.com>
2021-10-15 09:18:33 -0300

    Implementação do teste para gerar lista de imagens em base64

--- api-face-auth/Controllers/FaceController.cs
+++ api-face-auth/Controllers/FaceController.cs
@@ -25,11 +25,47 @@ namespace api_face_auth.Controllers
         }
 
         [HttpPost("register")]
-        public JsonResult registerFace([FromForm] IFormFile imagem)
+        public JsonResult registerFace([FromForm] List<IFormFile> imagens)
         {
-            Console.WriteLine("Imagem recebida");
-            return Json(new { message = "Cadastro do Rosto do usuário em construção"});
+            //Fazer tratamento para validar extensão das imagens
+            if(imagens.Count()==0){
+                return Json(new {message="Nenhuma imagem recebida"});
+            }
+            int qtdRecebida = imagens.Count();
+            List<string> nomesImgs = new List<string>();
+            foreach (var img in imagens)
+            {
+                nomesImgs.Add(img.FileName);
+            }
+
+            Console.WriteLine($"Imagem recebida ({qtdRecebida})");
+            return Json(new { qtdRecebida = qtdRecebida, nomes = nomesImgs.ToArray(), message = "Cadastro do Rosto do usuário em construção"});
         }
+
+        [HttpPost("register64")]
+        public JsonResult register64Face(string imagem){
+            //Fazer tratamento para validar extensão das imagens
+            if(imagens == null){
+                Console.WriteLine("Requisição register64> nada recebido");
+                return Json(new {message = "Parametro vazio"});
+            }
+
+            Byte[] bytesImg = Convert.FromBase64String(imagem);
+
+            if(bytesImg.Length > 0){
+                using (var stream = new FileStream(arquivo, FileMode.Create)){
+                    stream.Write(bytesImg,0,bytesImg.Length);
+                    stream.Flush();
+                }
+            }
+            
+
+            Console.WriteLine("Requisição register64 chegou no server");
+
+
+            return Json(new { message = "Rota para receber imagens em base 64"});
+        }
+
 
         [HttpGet("consultar/{id}")]
         public JsonResult GetFace(int id)
