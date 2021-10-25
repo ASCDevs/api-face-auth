58dcececd709f5c9c60e5ecdf9d16aea386bd0e9
Alexandre Santos <ale.santos49@gmail.com>
2021-10-25 14:52:27 -0300

    Esboço inicial para implemnetar Controller para o EmguCV realizar o treinamento e reconhecimento de face

---
+++ api-face-auth/Config.cs
@@ -0,0 +1,9 @@
+namespace api_face_auth
+{
+    public static class Config{
+        public static string HaarCascadePath = "Resources\\haarcascade_frontalface_default.xml";
+        public static string ImageFileExtension = ".jpg";
+        public static string FacePhotosPath = "Source\\FacesUploads\\";
+
+    }
+}
