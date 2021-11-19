8c4868f4a66b625e37bbc856b5cbbe7238663dce
Alexandre Santos <ale.santos49@gmail.com>
2021-11-18 23:59:01 -0300

    Padronizando o uso do bd com o EntityFramework

--- api-face-auth/DataSqlite/Manager/ContextManager.cs
+++ api-face-auth/DataSqlite/Manager/ContextManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using api_face_auth.DataSqlite.Entities;
+using api_face_auth.DataSqlite.Store;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,20 @@ namespace api_face_auth.DataSqlite.Manager
 {
     public class ContextManager
     {
+        private ContextStore _store;
+        public ContextManager(ContextStore store)
+        {
+            _store = store;
+        }
+
+        public int CreateUser(User newUser)
+        {
+            return _store.CreateUser(newUser);
+        }
+
+        public void CreateUserFaceInfo(UserFace newFaceInfo)
+        {
+            _store.CreateUser(newFaceInfo);
+        }
     }
 }
--- api-face-auth/DataSqlite/Store/ContextStore.cs
+++ api-face-auth/DataSqlite/Store/ContextStore.cs
@@ -1,11 +1,100 @@
-﻿using System;
+﻿using api_face_auth.DataSqlite.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api_face_auth.DataSqlite.Store
 {
+    
     public class ContextStore
     {
+        private readonly AppDbContext _context;
+        public ContextStore(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public int CreateUser(User newUser)
+        {
+            try
+            {
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+                return newUser.idUser;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Erro ao cadastrar user: "+e.Message);
+            }
+        }
+
+        public void CreateUserFaceInfo(List<UserFace> faceInfoList)
+        {
+            try
+            {
+                _context.UsersFace.AddRange(faceInfoList);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Erro ao casdastar informações faciais: "+e.Message);
+            }
+
+        }
+
+        public void CreateArticle()
+        {
+
+        }
+
+        public void UpdateUser()
+        {
+
+        }
+
+        public void UpdateUserFaceInfo()
+        {
+
+        }
+
+        public void UpdateArticle()
+        {
+
+        }
+
+        public void DeleteUser()
+        {
+
+        }
+
+        public void DeleteUserFaceInfo()
+        {
+
+        }
+
+        public void DeleteArticle()
+        {
+
+        }
+
+        public void GetArticles()
+        {
+
+        }
+
+        public void GetUserByUsername()
+        {
+
+        }
+
+        public void GetUserFaceInfo()
+        {
+
+        }
+
+
+        
     }
 }
