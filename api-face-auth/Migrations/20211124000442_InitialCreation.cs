23830cd05b3c3a354e16898ef22469e14faf381d
Alexandre Santos <ale.santos49@gmail.com>
2021-11-23 21:26:58 -0300

    Arquivos relacionados ao migrations e sqlite

---
+++ api-face-auth/Migrations/20211124000442_InitialCreation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_face_auth.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    idUser = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    surname = table.Column<string>(type: "TEXT", nullable: true),
+                    username = table.Column<string>(type: "TEXT", nullable: true),
+                    password = table.Column<string>(type: "TEXT", nullable: true),
+                    email = table.Column<string>(type: "TEXT", nullable: true),
+                    registerDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.idUser);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersFace",
+                columns: table => new
+                {
+                    idface = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    iduser = table.Column<int>(type: "INTEGER", nullable: false),
+                    image = table.Column<byte[]>(type: "BLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersFace", x => x.idface);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "UsersFace");
+        }
+    }
+}
